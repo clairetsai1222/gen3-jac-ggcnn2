ea361c3cf01f069cfe9a2bd3809c5f0367a531b9
clairetsai1222 <claire.tsai12@outlook.com>
2024-09-10 15:25:19 +0800

    get pose

--- 01-ori_grasp_pose.py
+++ 01-ori_grasp_pose.py
@@ -18,7 +18,15 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 import torch
+import matplotlib.pyplot as plt
+import datetime
+
+from models.ggcnn import GGCNN
 from models.ggcnn2 import GGCNN2
+from ggcnn_torch import predict, process_depth_image
+
+from utils.dataset_processing import grasp, grocess_output
+
 
 # 将相机坐标转换为机械臂坐标系
 def camera_to_robot_frame(camera_point, T_cam2gripper, intrinsics, depth_scale):
@@ -64,14 +72,6 @@ def get_camera_intrinsics():
 
 # 加载相机内参
 depth_intrinsic_matrix, color_intrinsic_matrix, depth_scale = get_camera_intrinsics()
-# 加载模型
-# Option 1) Load the model directly.
-# (this may print warning based on the installed version of python)
-model = torch.load('ggcnn_weights_jacquard/epoch_100_iou_0.97')
-
-# Option 2) Instantiate a model and load the weights.
-model = GGCNN2()
-model.load_state_dict(torch.load('ggcnn_weights_jacquard/epoch_100_iou_0.97_statedict.pt'))
 
 # 配置Realsense摄像头
 pipeline = rs.pipeline()
@@ -81,8 +81,9 @@ config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 # 启动摄像头
 pipeline.start(config)
 
+stop_flag = True
 try:
-    while True:
+    while stop_flag:
         # 获取图像帧
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
@@ -93,29 +94,55 @@ try:
 
         # 将图像转换为numpy数组
         depth_image = np.asanyarray(depth_frame.get_data())
+        
         color_image = np.asanyarray(color_frame.get_data())
 
         # 预处理深度图像（假设模型需要归一化）
-        depth_image_normalized = depth_image.astype(np.float32) / 65535.0  # 归一化到[0, 1]
-        depth_image_tensor = torch.tensor(depth_image_normalized, dtype=torch.float32).unsqueeze(0).unsqueeze(0)  # 增加batch和channel维度
+        # depth_image_normalized = depth_image.astype(np.float32) / 65535.0  # 归一化到[0, 1]
+        # depth_image_tensor = torch.tensor(depth_image_normalized, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+
+        model = GGCNN()
+        # model.load_state_dict(torch.load('ggcnn_weights_jacquard/epoch_100_iou_97_statedict.pt'))
+        model.load_state_dict(torch.load('ggcnn_weights_cornell/ggcnn_epoch_23_cornell_statedict.pt'))
 
         # 使用模型预测抓取点
         with torch.no_grad():
-            grasp_point = model(depth_image_tensor)
+            depthT = torch.from_numpy(depth_image.reshape(1, 1, 848, 480).astype(np.float32))
+            grasp_imgs = model(depthT)
 
-        # 假设模型输出的是抓取点的坐标 (x, y)
-        grasp_point = grasp_point.squeeze().numpy()
-        x, y = int(grasp_point[0]), int(grasp_point[1])
+        q_img, ang_img, width_img = grocess_output.post_process_output(q_img = grasp_imgs[0], cos_img = grasp_imgs[1], sin_img = grasp_imgs[2], width_img=grasp_imgs[3])
+        grasps = grasp.detect_grasps (q_img = q_img, ang_img=ang_img, width_img=width_img, no_grasps=1)
 
-        # 在深度图像和彩色图像上绘制抓取点
+        # 模型输出一个抓取的框架，需要将其转换为机械臂坐标系下的抓取点
+        for grasp_objects in grasps:
+            horizon_angle = grasp_objects.as_gr.angle
+            grasp_point = grasp_objects.as_gr.as_grasp
+            rectangle_center = grasp_objects.as_gr.center
+            rectengle_length = grasp_objects.as_gr.length
+            rectengle_width = grasp_objects.as_gr.width
+            polygon_points = grasp_objects.as_gr.polygon_coords
+            print("grap point in camera frame:", rectangle_center)
+
+        # # 在深度图像和彩色图像上绘制抓取点
+        x, y = rectangle_center[0], rectangle_center[1]
         depth_image_color = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
         cv2.circle(depth_image_color, (x, y), 5, (0, 255, 0), -1)
         cv2.circle(color_image, (x, y), 5, (0, 255, 0), -1)
 
-        # 显示图像
         cv2.imshow('Depth Image with Grasp Point', depth_image_color)
         cv2.imshow('Color Image with Grasp Point', color_image)
 
+        # 获取当前系统时间并格式化为字符串
+        current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        # 保存深度图像
+        depth_image_path = f'./grasp_output/ori_grasp_image/depth_image_{current_time}.png'  # 指定保存路径
+        cv2.imwrite(depth_image_path, depth_image_color)
+
+        # 保存彩色图像
+        color_image_path = f'./grasp_output/ori_grasp_image/color_image_{current_time}.png'  # 指定保存路径
+        cv2.imwrite(color_image_path, color_image)
+        
         # 按下'q'键退出
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
---
+++ ggcnn_torch.py
@@ -0,0 +1,109 @@
+from os import path
+import sys
+
+import cv2
+import numpy as np
+import scipy.ndimage as ndimage
+
+import torch
+
+#from models.ggcnn import GGCNN
+from models.ggcnn2 import GGCNN2
+
+from gen3_utils.timeit import TimeIt
+
+device = torch.device("cuda:0")
+
+# 加载模型
+# Option 2) Instantiate a model and load the weights.
+model = GGCNN2().to(device)
+# model.load_state_dict(torch.load('ggcnn_weights_cornell/ggcnn_epoch_23_cornell_statedict.pt'))
+model.load_state_dict(torch.load('ggcnn_weights_jacquard/epoch_100_iou_97_statedict.pt'))
+
+
+def process_depth_image(depth, crop_size, out_size=300, return_mask=False, crop_y_offset=0):
+    imh, imw = depth.shape
+
+    with TimeIt('1'):
+        # Crop.
+        depth_crop = depth[(imh - crop_size) // 2 - crop_y_offset:(imh - crop_size) // 2 + crop_size - crop_y_offset,
+                           (imw - crop_size) // 2:(imw - crop_size) // 2 + crop_size]
+    # depth_nan_mask = np.isnan(depth_crop).astype(np.uint8)
+
+    depth_crop = np.array(depth_crop)
+    print(f'Type of depth_crop: {type(depth_crop)}')
+    print(f'Shape of depth_crop: {depth_crop.shape}')
+
+
+    # Inpaint
+    # OpenCV inpainting does weird things at the border.
+    with TimeIt('2'):
+        depth_crop = cv2.copyMakeBorder(depth_crop, 1, 1, 1, 1, cv2.BORDER_DEFAULT)
+        depth_nan_mask = np.isnan(depth_crop).astype(np.uint8)
+
+    with TimeIt('3'):
+        depth_crop[depth_nan_mask==1] = 0
+
+    with TimeIt('4'):
+        # Scale to keep as float, but has to be in bounds -1:1 to keep opencv happy.
+        depth_scale = np.abs(depth_crop).max()
+        depth_crop = depth_crop.astype(np.float32) / depth_scale  # Has to be float32, 64 not supported.
+
+        with TimeIt('Inpainting'):
+            depth_crop = cv2.inpaint(depth_crop, depth_nan_mask, 1, cv2.INPAINT_NS)
+
+        # Back to original size and value range.
+        depth_crop = depth_crop[1:-1, 1:-1]
+        depth_crop = depth_crop * depth_scale
+
+    with TimeIt('5'):
+        # Resize
+        depth_crop = cv2.resize(depth_crop, (out_size, out_size), cv2.INTER_AREA)
+
+    if return_mask:
+        with TimeIt('6'):
+            depth_nan_mask = depth_nan_mask[1:-1, 1:-1]
+            depth_nan_mask = cv2.resize(depth_nan_mask, (out_size, out_size), cv2.INTER_NEAREST)
+        return depth_crop, depth_nan_mask
+    else:
+        return depth_crop
+
+
+def predict(depth, process_depth=True, crop_size=480, out_size=480, depth_nan_mask=None, crop_y_offset=0, filters=(2.0, 1.0, 1.0)):
+    if process_depth:
+        depth, depth_nan_mask = process_depth_image(depth, crop_size, out_size=out_size, return_mask=True, crop_y_offset=crop_y_offset)
+
+    # Inference
+    depth = np.clip((depth - depth.mean()), -1, 1)
+    depthT = torch.from_numpy(depth.reshape(1, 1, out_size, out_size).astype(np.float32)).to(device)
+    with torch.no_grad():
+        pred_out = model(depthT)
+
+    points_out = pred_out[0].cpu().numpy().squeeze()
+    points_out[depth_nan_mask] = 0
+
+    # Calculate the angle map.
+    cos_out = pred_out[1].cpu().numpy().squeeze()
+    sin_out = pred_out[2].cpu().numpy().squeeze()
+    ang_out = np.arctan2(sin_out, cos_out) / 2.0
+
+    width_out = pred_out[3].cpu().numpy().squeeze() * 150.0  # Scaled 0-150:0-1
+
+    # Filter the outputs.
+    if filters[0]:
+        points_out = ndimage.filters.gaussian_filter(points_out, filters[0])  # 3.0
+    if filters[1]:
+        ang_out = ndimage.filters.gaussian_filter(ang_out, filters[1])
+    if filters[2]:
+        width_out = ndimage.filters.gaussian_filter(width_out, filters[2])
+
+    points_out = np.clip(points_out, 0.0, 1.0-1e-3)
+
+    # SM
+    # temp = 0.15
+    # ep = np.exp(points_out / temp)
+    # points_out = ep / ep.sum()
+
+    # points_out = (points_out - points_out.min())/(points_out.max() - points_out.min())
+
+    return points_out, ang_out, width_out, depth.squeeze()
