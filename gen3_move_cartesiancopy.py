42bf427dcb90189c60bd2cc2f59cbf5f305285a9
G <guo_yuao@sjtu.edu.cn>
2024-12-13 21:20:10 +0800

    api update

--- gen3_move_cartesiancopy.py
+++ gen3_move_cartesiancopy.py
@@ -17,6 +17,7 @@ import os
 import time
 import threading
 
+from collections.abc import Iterable
 from kortex_api.autogen.client_stubs.BaseClientRpc import BaseClient
 from kortex_api.autogen.client_stubs.BaseCyclicClientRpc import BaseCyclicClient
 
@@ -24,6 +25,30 @@ from kortex_api.autogen.messages import Base_pb2, BaseCyclic_pb2, Common_pb2
 
 # Maximum allowed waiting time during actions (in seconds)
 TIMEOUT_DURATION = 20
+
+def check_for_sequence_end_or_abort(e):
+    """Return a closure checking for END or ABORT notifications on a sequence
+
+    Arguments:
+    e -- event to signal when the action is completed
+        (will be set when an END or ABORT occurs)
+    """
+
+    def check(notification, e = e):
+        event_id = notification.event_identifier
+        task_id = notification.task_index
+        if event_id == Base_pb2.SEQUENCE_TASK_COMPLETED:
+            print("Sequence task {} completed".format(task_id))
+        elif event_id == Base_pb2.SEQUENCE_ABORTED:
+            print("Sequence aborted with error {}:{}"\
+                .format(\
+                    notification.abort_details,\
+                    Base_pb2.SubErrorCodes.Name(notification.abort_details)))
+            e.set()
+        elif event_id == Base_pb2.SEQUENCE_COMPLETED:
+            print("Sequence completed.")
+            e.set()
+    return check
 
 # Create closure to set an event after an END or an ABORT 
 # 这个函数返回一个闭包，用于检查动作是否结束（END）或中止（ABORT），并在检测到这些事件时设置一个线程事件e
@@ -79,13 +104,54 @@ def example_move_to_home_position(base):
     return finished
 
 
-def example_cartesian_action_movement(base, base_cyclic, point):
+def example_cartesian_action_movement_sequence(base, base_cyclic, action_list):
     '''
     Arg:
     base: an instance of BaseClient 基础客户端
     base_cyclic: an instance of BaseCyclicClient 循环基础客户端
     '''
+    # 按顺序调用action_list内部的动作
+    # 每一个 action in action_list 的前三位为point，后三位为angle
+    print("Creating Sequence")
+    action_sequence = Base_pb2.Sequence()
+    action_sequence.name = "Example sequence"
+    task = []
     
+    print("Appending Actions to Sequence")
+    if isinstance(action_sequence[0], Iterable):
+        for i, target in enumerate(action_sequence):
+            action = create_cartesian_action(base, base_cyclic, target)
+            task.append(action_sequence.tasks.add())
+            task[i].group_identifier = i
+            task[i].action.CopyFrom(action)
+    else:
+        action = create_cartesian_action(base, base_cyclic, action_sequence)
+        task.append(action_sequence.tasks.add())
+        task[i].group_identifier = i
+        task[i].action.CopyFrom(action)
+
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationSequenceActionTopic(
+        check_for_sequence_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+    print("Creating sequence on device and executing it")
+    handle_sequence = base.CreateSequence(action_sequence)
+    base.PlaySequence(handle_sequence)
+
+    print("Waiting for movement to finish ...")
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+    task.clear()
+
+    if finished:
+        print("Cartesian movement completed")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+
+def create_cartesian_action(base, base_cyclic, target):
     print("Starting Cartesian action movement ...")
     action = Base_pb2.Action()
     action.name = "Example Cartesian action movement"
@@ -94,13 +160,19 @@ def example_cartesian_action_movement(base, base_cyclic, point):
     feedback = base_cyclic.RefreshFeedback() # 从base_cyclic客户端获取当前的反馈信息
 
     cartesian_pose = action.reach_pose.target_pose
+
+    cartesian_pose.x = (target[0]+3)     *0.01     # (meters) 
+    cartesian_pose.y = (target[1]+20)     *0.01   # (meters)
+    cartesian_pose.z = target[2]     *0.01   # (meters)
     
-    cartesian_pose.x = (point[0]+3)     *0.01     # (meters) 
-    cartesian_pose.y = (point[1]+20)     *0.01   # (meters)
-    cartesian_pose.z = point[2]     *0.01   # (meters)
-    cartesian_pose.theta_x = feedback.base.tool_pose_theta_x # (degrees)夹爪角度：+往下；-往上
-    cartesian_pose.theta_y = feedback.base.tool_pose_theta_y # (degrees)夹爪角度：+逆时针；-顺时针
-    cartesian_pose.theta_z = feedback.base.tool_pose_theta_z # (degrees)夹爪角度：+左转；-右转
+    if len(target) == 6:
+        cartesian_pose.theta_x = target[3] # (degrees)夹爪角度：+往下；-往上
+        cartesian_pose.theta_y = target[4] # (degrees)夹爪角度：+逆时针；-顺时针
+        cartesian_pose.theta_z = target[5] # (degrees)夹爪角度：+左转；-右转
+    else:
+        cartesian_pose.theta_x = feedback.base.tool_pose_theta_x # (degrees)夹爪角度：+往下；-往上
+        cartesian_pose.theta_y = feedback.base.tool_pose_theta_y # (degrees)夹爪角度：+逆时针；-顺时针
+        cartesian_pose.theta_z = feedback.base.tool_pose_theta_z # (degrees)夹爪角度：+左转；-右转
 
     # cartesian_pose.x = feedback.base.tool_pose_x 
     # cartesian_pose.y = feedback.base.tool_pose_y 
@@ -109,26 +181,15 @@ def example_cartesian_action_movement(base, base_cyclic, point):
     # cartesian_pose.theta_y = feedback.base.tool_pose_theta_y # (degrees)夹爪角度：+逆时针；-顺时针
     # cartesian_pose.theta_z = feedback.base.tool_pose_theta_z # (degrees)夹爪角度：+左转；-右转
 
-    e = threading.Event()
-    notification_handle = base.OnNotificationActionTopic(
-        check_for_end_or_abort(e),
-        Base_pb2.NotificationOptions()
-    )
-    print("Executing action")
-    base.ExecuteAction(action)
+    return action
 
-    print("Waiting for movement to finish ...")
-    finished = e.wait(TIMEOUT_DURATION)
-    base.Unsubscribe(notification_handle)
 
-    if finished:
-        print("Cartesian movement completed")
-    else:
-        print("Timeout on action notification wait")
-    return finished
-
-def move_action(point):
-    
+def move_action(action_list):
+    """
+    Input: action_list
+    [x, y, z] or [x, y, z, theta_x, theta_y, theta_z]
+    or a list composed of the first two, and the actions will be executed in order.
+    """
     # Import the utilities helper module
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
     import utilities
@@ -147,7 +208,7 @@ def move_action(point):
         success = True
 
         success &= example_move_to_home_position(base)
-        success &= example_cartesian_action_movement(base, base_cyclic, point)
+        success &= example_cartesian_action_movement_sequence(base, base_cyclic, action_list)
 
         # You can also refer to the 110-Waypoints examples if you want to execute
         # a trajectory defined by a series of waypoints in joint space or in Cartesian space
---
+++ meta_action.py
@@ -0,0 +1,5 @@
+import gen3_move_cartesiancopy as move
+# grasp
+# move to
+# dump
+# put back
